a623348ef0b7582e3e5d94a341c385511eb9c8b7
Luke Patrick DC (Dela Cruz) <32045517+lukepatr@users.noreply.github.com>
2018-01-10 22:27:19 -0700

    Delete sorting.h

--- RestauFinder/sorting.h
+++
@@ -1,8 +0,0 @@
-#ifndef _POWMOD_H_
-#define _POWMOD_H_
-
-void swap_rest(RestDist *ptr_rest1, RestDist *ptr_rest2);
-int pivot(RestDist *rest_dist, int start, int end);
-void qsort(RestDist *rest_dist, int start, int end);
-
-#endif
